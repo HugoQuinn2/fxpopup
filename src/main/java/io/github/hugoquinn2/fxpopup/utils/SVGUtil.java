5623ab3bd28846458ab0cc38618271ecd0768cd8
HugoQuinn <hugoquinn2@gmail.com>
2024-12-27 14:35:37 -0600

    add comments to SVGUtil.java

--- src/main/java/io/github/hugoquinn2/fxpopup/utils/SVGUtil.java
+++ src/main/java/io/github/hugoquinn2/fxpopup/utils/SVGUtil.java
@@ -8,15 +8,26 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.SVGPath;
 
 public class SVGUtil {
+    /**
+     * Load .svg ico from resources.
+     * @param fxPopIcon FxPopIcon to load.
+     * @param scale Double value scale to apply.
+     */
     public static SVGPath getIcon(FxPopIcon fxPopIcon, double scale) {
         if (fxPopIcon.equals(FxPopIcon.NOTHING))
             return null;
 
-        String svgPathContent = SVGLoader.loadSVGFromResources(String.format(FxPopupConfig.iconPath, fxPopIcon.literalIcon));
+        // Extract svg code from .svg file
+        String svgPathContent = SVGLoader.loadSVGFromResources(
+                String.format(FxPopupConfig.iconPath, fxPopIcon.literalIcon)
+        );
+
         SVGPath svg = new SVGPath();
         svg.setContent(svgPathContent);
         svg.setScaleX(scale);
         svg.setScaleY(scale);
+
+        // Add ICON class
         svg.getStyleClass().add(StyleConfig.ICON);
 
         return svg;
