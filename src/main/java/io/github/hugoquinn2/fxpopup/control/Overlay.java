b4bae2995d4a696ec24c7b90c5b77868d61b0bb5
hugoq <hugoquinn2@gmail.com>
2025-02-14 23:58:33 -0600

    📝 doc(comments Overlay): add doc comments to Overlay.java

--- src/main/java/io/github/hugoquinn2/fxpopup/control/Overlay.java
+++ src/main/java/io/github/hugoquinn2/fxpopup/control/Overlay.java
@@ -10,10 +10,32 @@ import javafx.scene.shape.Rectangle;
 import static io.github.hugoquinn2.fxpopup.config.CssClasses.OVERLAY_CLASS;
 
 public class Overlay extends Rectangle {
+    /**
+     * Create an Overlay with default background color <code>Color.BLACK</code> and default opacity <code>0.3</code>.
+     * The overlay will be linked to the root of the scene.
+     */
     public Overlay(){this(Color.BLACK, 0.3);}
+
+    /**
+     * Create an Overlay with custom background color and default opacity <code>0.3</code>.
+     * The overlay will be linked to the root of the scene.
+     * @param color the background color.
+     */
     public Overlay(Color color){this(color, 0.3);}
+
+    /**
+     * Create an Overlay with default background color <code>Color.BLACK</code> and custom opacity.
+     * The overlay will be linked to the root of the scene.
+     * @param opacity the opacity value.
+     */
     public Overlay(double opacity){this(Color.BLACK, opacity);}
 
+    /**
+     * Create an Overlay with custom background color and custom opacity.
+     * The overlay will be linked to the root of the scene.
+     * @param color the background color.
+     * @param opacity the opacity value.
+     */
     public Overlay(Color color, double opacity) {
         this.setFill(color);
         this.setOpacity(opacity);
