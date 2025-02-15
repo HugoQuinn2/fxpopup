5e9933d2c6d99e629038437b364633f22cdd8c28
hugoq <hugoquinn2@gmail.com>
2025-02-15 13:22:56 -0600

    📝 doc(Overlay.java): add doc comments to constructors.

--- src/main/java/io/github/hugoquinn2/fxpopup/control/Overlay.java
+++ src/main/java/io/github/hugoquinn2/fxpopup/control/Overlay.java
@@ -1,7 +1,5 @@
 package io.github.hugoquinn2.fxpopup.control;
 
-import io.github.hugoquinn2.fxpopup.utils.MasterUtils;
-import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -14,21 +12,27 @@ public class Overlay extends Rectangle {
      * Create an Overlay with default background color <code>Color.BLACK</code> and default opacity <code>0.3</code>.
      * The overlay will be linked to the root of the scene.
      */
-    public Overlay(){this(Color.BLACK, 0.3);}
+    public Overlay() {
+        this(Color.BLACK, 0.3);
+    }
 
     /**
      * Create an Overlay with custom background color and default opacity <code>0.3</code>.
      * The overlay will be linked to the root of the scene.
      * @param color the background color.
      */
-    public Overlay(Color color){this(color, 0.3);}
+    public Overlay(Color color) {
+        this(color, 0.3);
+    }
 
     /**
      * Create an Overlay with default background color <code>Color.BLACK</code> and custom opacity.
      * The overlay will be linked to the root of the scene.
      * @param opacity the opacity value.
      */
-    public Overlay(double opacity){this(Color.BLACK, opacity);}
+    public Overlay(double opacity) {
+        this(Color.BLACK, opacity);
+    }
 
     /**
      * Create an Overlay with custom background color and custom opacity.
