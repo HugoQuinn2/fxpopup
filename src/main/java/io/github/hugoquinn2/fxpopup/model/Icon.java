89a91575f157fc592efa3f4e32ab9031328a2da8
vgaspar <hugoquinn2@gmail.com>
2025-02-24 17:17:18 -0600

    ✨ feature(getters and setters): add setters and getters to Icon.java

--- src/main/java/io/github/hugoquinn2/fxpopup/model/Icon.java
+++ src/main/java/io/github/hugoquinn2/fxpopup/model/Icon.java
@@ -1,19 +1,47 @@
 package io.github.hugoquinn2.fxpopup.model;
 
-import io.github.hugoquinn2.fxpopup.config.FxPopupConfig;
 import io.github.hugoquinn2.fxpopup.constants.FxPopIcon;
 import io.github.hugoquinn2.fxpopup.utils.SVGUtil;
 import javafx.scene.control.Label;
 
 public class Icon extends Label {
+    private FxPopIcon icon;
+    private double scale = 1;
+
     public Icon(){
     }
 
     public Icon(FxPopIcon icon) {
-        this.setGraphic(SVGUtil.getIcon(icon, FxPopupConfig.iconScale));
+        this(icon, 1);
     }
 
     public Icon(FxPopIcon icon, double scale) {
         this.setGraphic(SVGUtil.getIcon(icon, scale));
+    }
+
+    public void setIcon(FxPopIcon icon) {
+        this.icon = icon;
+        this.setGraphic(SVGUtil.getIcon(icon, scale));
+    }
+
+    public void setScale(double scale) {
+        this.scale = scale;
+        this.setGraphic(SVGUtil.getIcon(icon, scale));
+    }
+
+    public FxPopIcon getIcon() {
+        return icon;
+    }
+
+    public double getScale() {
+        return scale;
+    }
+
+    @Override
+    public String toString() {
+        return "Icon{" +
+                "icon=" + icon +
+                ", scale=" + scale +
+                '}';
     }
 }
