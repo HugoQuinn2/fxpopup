840f9ceafe92e311b4d79f3aff37f15498fa8bb2
vgaspar <hugoquinn2@gmail.com>
2025-02-24 10:24:52 -0600

    ⚰️ Remove unused ToolTip.java classes

--- src/main/java/io/github/hugoquinn2/fxpopup/config/StyleConfig.java
+++ src/main/java/io/github/hugoquinn2/fxpopup/config/StyleConfig.java
@@ -49,19 +49,4 @@ public class StyleConfig {
      * CSS class name for the label of a form field.
      */
     public static final String LABEL = "form-label-field";
-
-    /**
-     * CSS class name for tool tip parent
-     * */
-    public static final String TOOL_TIP = "tooltip";
-
-    /**
-     * CSS class name for tool tip text
-     * */
-    public static final String TOOL_TIP_TEXT = "tool-tip-text";
-
-    /**
-     * CSS class name for tool tip pointer
-     * */
-    public static final String TOOL_TIP_POINTER = "tool-tip-pointer";
 }
